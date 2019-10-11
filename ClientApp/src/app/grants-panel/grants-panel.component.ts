638ae9a6ca47c4c5225a3d584f2e2898db3b7d50
Branden Coker <branden.coker@gmail.com>
2019-10-10 21:02:16 -0500

    updated grant-panel component to pull back mock data

--- ClientApp/src/app/grants-panel/grants-panel.component.ts
+++ ClientApp/src/app/grants-panel/grants-panel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Grant } from '../Models/grant';
+import { Grants } from '../Data/mock-grants';
 @Component({
   selector: 'app-grants-panel',
   templateUrl: './grants-panel.component.html',
   styleUrls: ['./grants-panel.component.css']
 })
 export class GrantsPanelComponent implements OnInit {
+  grants = Grants;
+  selectedGrant: Grant;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(grant: Grant): void {
+    this.selectedGrant = grant;
+  }
 }
